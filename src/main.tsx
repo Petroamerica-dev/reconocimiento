cda9172df1e99d424a611388e35973bdc4eb6da7
AyoriaChagua <ayoriachagua@gmail.com>
2025-10-31 12:52:24 -0500

    add basename

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "@/context/AuthProvider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/reconocimiento">
       <AuthProvider>
         <App />
       </AuthProvider>
